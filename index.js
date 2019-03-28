65e3606e03a1d7a9ae110e5741f43f804b19e6ae
John Pospisil <audiofreak7@gmail.com>
2019-03-28 15:27:20 +0000

    Serve static files

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+// The next line makes is so that files in the 'views' & 'public' folders 
+// can be accessed without writing '/views' or '/public' in each file path,
+// for instance, in the 'app.get' line below.
+// '__dirname' holds the current directory's name.
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
     
 app.get('/', function(req, res) {
-    res.send("Hello from the Root Route!");
+    res.sendFile("index.html");
 });
 
 app.use('/api/todos', todoRoutes);
