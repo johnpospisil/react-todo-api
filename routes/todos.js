45d72e169f9c3f7c22dda838853b5d6ddb203a88
John Pospisil <audiofreak7@gmail.com>
2019-03-28 03:19:41 +0000

    Move most routing data to the 'helpers' folder

--- routes/todos.js
+++ routes/todos.js
@@ -1,60 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../models");
+var helpers = require("../helpers/todos");
 
-router.get('/', function(req, res) {
-    db.Todo.find()
-    .then(function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
+// GET and POST Routes
+router.route('/')
+    .get(helpers.getTodos)
+    .post(helpers.createTodo)
 
-router.post('/', function(req, res){
-    // res.send("THIS IS THE POST ROUTE!");
-    // console.log(req.body);
-    db.Todo.create(req.body)
-    .then(function(newTodo){
-        // also respond with status code 201 (i.e. 'created')
-        res.status(201).json(newTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-router.get('/:todoId', function(req, res){
-    db.Todo.findById(req.params.todoId)
-    .then(function(foundTodo){
-        res.json(foundTodo)
-    })
-});
-
-router.put('/:todoId', function(req, res){
-    db.Todo.findOneAndUpdate({_id:req.params.todoId}, req.body, {new: true})
-    .then(function(todo){
-        res.json(todo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-router.delete('/:todoId', function(req, res){
-    db.Todo.remove({_id: req.params.todoId})
-    .then(function(){
-        res.json({message: 'We deleted it!'});
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-
+// GET, PUT, and DELETE Routes
+router.route('/:todoId')
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
 
 module.exports = router;
