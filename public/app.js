8a5fad936c91d1ab1ef411d2f0c53430975a495d
John Pospisil <audiofreak7@gmail.com>
2019-03-28 16:25:27 +0000

    Writie initial AJAX call

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
-alert("Hi, from app.js!");
+/* global $ */
+$(document).ready(function(){
+   $.getJSON("/api/todos")
+   .then(addTodos)
+});
+
+function addTodos(todos) {
+    // add todos to page here
+    todos.forEach(function(todo){
+        var newTodo = $('<li class="task">' + todo.name + '</li>');
+        $('.list').append(newTodo);
+    })
+}
