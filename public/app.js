1584952b77c3b41355c4e5c2ce8d4b31d8c38c58
John Pospisil <audiofreak7@gmail.com>
2019-03-28 21:16:11 +0000

    Connect the form to the API

--- public/app.js
+++ public/app.js
@@ -2,15 +2,40 @@
 $(document).ready(function(){
    $.getJSON("/api/todos")
    .then(addTodos)
+   
+   $('#todoInput').keypress(function(event){
+    //   '13' is the event code for the 'ENTER' key
+       if(event.which == 13) {
+            // create todo
+            createTodo();
+       }
+   })
 });
 
 function addTodos(todos) {
     // add todos to page here
     todos.forEach(function(todo){
-        var newTodo = $('<li class="task">' + todo.name + '</li>');
+        addTodo(todo);
+    });
+}
+
+function createTodo(){
+    // send request to create new todo
+    var userInput = $('#todoInput').val();
+    $.post('/api/todos', {name: userInput})
+    .then(function(newTodo){
+        addTodo(newTodo);
+        $('#todoInput').val('');
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+
+function addTodo(todo){
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
         if(todo.completed){
           newTodo.addClass("done");  
         } 
         $('.list').append(newTodo);
-    })
 }
