f26e8f24c1479e12964c31281f224fa12bca93be
John Pospisil <audiofreak7@gmail.com>
2019-03-30 04:06:23 +0000

    Toggle todo items as complete or incomplete when clicked

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
    $.getJSON("/api/todos")
    .then(addTodos)
    
+   // CREATE TODO
    $('#todoInput').keypress(function(event){
     //   '13' is the event code for the 'ENTER' key
        if(event.which == 13) {
@@ -10,8 +11,18 @@ $(document).ready(function(){
             createTodo();
        }
    })
+   
+   // TOGGLE TODO 'completed' STATE
+   $('.list').on('click', 'li', function(){
+       updateTodo($(this));
+   })
+   
+   // DELETE TODO
    // Add a click listener to the spans within the 'list' class.
-   $('.list').on('click', 'span', function(){
+   $('.list').on('click', 'span', function(e){
+       // make sure that the code after this following line is not run
+       // when a todo is toggled.
+       e.stopPropagation();
        var clickedId = $(this).parent().data('id');
        var deleteUrl = '/api/todos/' + clickedId;
         // When X is clicked, remove the parent element  from the page
@@ -57,8 +68,27 @@ function addTodo(todo){
     // create a piece of data called 'id' that has the value of the newTodo db id.
     // This is useful when deleting todo itmes in the list.
     newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
     if(todo.completed){
         newTodo.addClass("done");  
     } 
     $('.list').append(newTodo);
+}
+
+function updateTodo(todo){
+    // send request to change the status on the server
+    // update view
+    var updateUrl = '/api/todos/' + todo.data('id');
+    var isDone = !todo.data('completed');
+    var updateData = {completed: isDone};
+    $.ajax({
+        method: 'PUT',
+        url: updateUrl,
+        data: updateData
+    })
+    .then(function(updatedTodo){
+        todo.toggleClass("done");
+        todo.data('completed', isDone);
+    })
+    
 }
