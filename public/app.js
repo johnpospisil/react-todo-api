6d11a522b196b47b2d3df7bbcc79e5fe1ee1f467
John Pospisil <audiofreak7@gmail.com>
2019-03-30 03:34:24 +0000

    Add 'delete todo' button and functionality

--- public/app.js
+++ public/app.js
@@ -10,7 +10,26 @@ $(document).ready(function(){
             createTodo();
        }
    })
+   // Add a click listener to the spans within the 'list' class.
+   $('.list').on('click', 'span', function(){
+       var clickedId = $(this).parent().data('id');
+       var deleteUrl = '/api/todos/' + clickedId;
+        // When X is clicked, remove the parent element  from the page
+        $(this).parent().remove();
+        // delete the todo from the database
+        $.ajax({
+            method: 'DELETE',
+            url: deleteUrl
+        })
+        .then(function(data){
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+    })
 });
+
+
 
 function addTodos(todos) {
     // add todos to page here
@@ -25,6 +44,7 @@ function createTodo(){
     $.post('/api/todos', {name: userInput})
     .then(function(newTodo){
         addTodo(newTodo);
+        // clear the input field after the newTodo is added to the list
         $('#todoInput').val('');
     })
     .catch(function(err){
@@ -33,9 +53,12 @@ function createTodo(){
 }
 
 function addTodo(todo){
-    var newTodo = $('<li class="task">' + todo.name + '</li>');
-        if(todo.completed){
-          newTodo.addClass("done");  
-        } 
-        $('.list').append(newTodo);
+    var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
+    // create a piece of data called 'id' that has the value of the newTodo db id.
+    // This is useful when deleting todo itmes in the list.
+    newTodo.data('id', todo._id);
+    if(todo.completed){
+        newTodo.addClass("done");  
+    } 
+    $('.list').append(newTodo);
 }
